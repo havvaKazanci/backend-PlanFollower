bf85f0088bca674b96e03cc550702fa642f28844
havvaKazanci <h.caglakazanci@gmail.com>
2026-01-11 13:54:08 +0300

    feat: implement user login and JWT generation

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 const pool = require('../config/db'); // database connection
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken'); //token create
 
 const register = async (req, res) => {
     //destructing the android datas
@@ -38,3 +39,48 @@ const register = async (req, res) => {
 };
 
 module.exports = { register };
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        //Find if there a same email
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+
+        //401, No user like that
+        if (user.rows.length === 0) {
+            return res.status(401).json({ message: "Invalid Email or Password" });
+        }
+
+        //consider passwords with hash type: bcrypt
+        const isMatch = await bcrypt.compare(password, user.rows[0].password_hash);
+
+        if (!isMatch) {
+            return res.status(401).json({ message: "Invalid Email or Password" });
+        }
+
+        //When everything is ok, than create a JWT
+        const token = jwt.sign(
+            { userId: user.rows[0].id }, 
+            process.env.JWT_SECRET, 
+            { expiresIn: '1d' } // Token 1 gün boyunca geçerli olsun
+        );
+
+        //Give result about info user
+        res.status(200).json({
+            message: "Login succesfull",
+            token: token,
+            user: {
+                id: user.rows[0].id,
+                name: user.rows[0].name,
+                email: user.rows[0].email
+            }
+        });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+module.exports = { register, login }; 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController');
 
 // "POST /api/auth/register" calling register function which is in the controller
 router.post('/register', authController.register);
+router.post('/login', authController.login);
 
 module.exports = router;
