702d0e3fb66d1b5dfac8dc5429fd5b3e50d38d15
havvaKazanci <h.caglakazanci@gmail.com>
2026-01-17 12:38:55 +0300

    feat: implement user-specific note retrieval logic
    - Add 'getUserNotes' controller to fetch notes belonging to the authenticated user
    - Define a new GET route in 'noteRoutes' for fetching notes
    - Ensure retrieved notes are sorted by creation date in descending order
    - Integrate user identity from authMiddleware to filter database queries

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -20,3 +20,23 @@ exports.createNote = async (req, res) => {
         res.status(500).send("Server error.");
     }
 };
+
+
+// getting all notes for the logged-in user
+exports.getUserNotes = async (req, res) => {
+    // uuid comes from middleware (for security used middleware)
+    const userId = req.user.userId; 
+
+    try {
+        // fetch notes belonging to this user, sorted by newest first
+        const notes = await pool.query(
+            "SELECT * FROM notes WHERE user_id = $1 ORDER BY created_at DESC",
+            [userId]
+        );
+
+        res.status(200).json(notes.rows);
+    } catch (err) {
+        console.error("Fetch Notes Error:", err.message);
+        res.status(500).json({ error: "Server error while fetching notes" });
+    }
+};
--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -6,4 +6,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 // POST /api/notes - Adding note (before adding you must loged in!)
 router.post('/', authMiddleware, noteController.createNote);
 
+// GET /api/notes - Retrieve user-specific notes
+router.get('/', authMiddleware, noteController.getUserNotes);
+
 module.exports = router;
